e8300a34f74781a626f688938560b7f3ae3aa6ef
Jeremy <ngyzj95@gmail.com>
2022-01-19 19:18:44 +0800

    added some configurations for personal use

---
+++ create_venv.bat
@@ -0,0 +1,3 @@
+@REM https://stackoverflow.com/questions/1534210/use-different-python-version-with-virtualenv
+@REM virtualenv --python={/usr/bin/python2.6} {<path/to/new/virtualenv/>}
+virtualenv --python=%1 %2
